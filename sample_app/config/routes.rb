ca5124b7e081a61c646ebb9bbf01b313a41e3088
大川亮一 <ryoichi@ookawaryouichinoMacBook-Air.local>
2020-02-06 14:17:03 +0900

    [Finish]3-7

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -11,4 +11,5 @@ get 'todolists/:id' => 'todolists#show', as: 'todolist'
 
 get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
 
+patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 end
