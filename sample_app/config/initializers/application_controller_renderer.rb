2c72e6fb19d9a2a59bbceae424732daf1dedd6a4
大川亮一 <ryoichi@ookawaryouichinoMacBook-Air.local>
2020-02-06 15:03:32 +0900

    [Finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '72b1b6dfaf697d9b8a8917f84bb608b7f1865100941a07983f05952ab0105cdc8df58eff241ad83b343de411a6f7810774af3078dbdc0a7431f30e34803d9268'
