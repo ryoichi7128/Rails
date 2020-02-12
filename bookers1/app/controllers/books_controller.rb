b743bb7e4341d0885d0f4bc3f5848d5c670c5ced
大川亮一 <ryoichi@ookawaryouichinoMacBook-Air.local>
2020-02-12 14:42:34 +0900

    課題２提出

---
+++ bookers1/app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+
+
+  def index
+    @books = Book.all
+
+
+  	@book = Book.new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+      flash[:notice] = "successfully"
+  	redirect_to book_path(@book.id)
+    else
+     render ("books/_new")
+   end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+    redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
+  end
+
+private
+
+def book_params
+	params.require(:book).permit(:title, :body)
+end
+
+end
