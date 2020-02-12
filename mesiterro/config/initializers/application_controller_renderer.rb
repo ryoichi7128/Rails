5303731c5472da04d0fef0468b45bc9f3a6c3300
大川亮一 <ryoichi@ookawaryouichinoMacBook-Air.local>
2020-02-12 18:42:38 +0900

    [Finish]5-3

--- mesiterro/config/initializers/application_controller_renderer.rb
+++ mesiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e81c9fe80e320db123efdf677174a1fc77752fcf930de923d022b0680dae4c78a1793443f5faa13c18cee025cce1ebced22c529b16181de833aacaaf01a436e6'
